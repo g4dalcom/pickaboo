2e7c6fa185c6125119f01b22880b6acebc494356
g4dalcom <g4dalcom@gmail.com>
2023-06-03 22:50:01 +0900

    chore: build.gradle 세팅
    - restdocs 관련 세팅

---
+++ server/pickaboo/src/main/java/com/project/pickaboo/PickabooApplication.java
@@ -0,0 +1,13 @@
+package com.project.pickaboo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PickabooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PickabooApplication.class, args);
+	}
+
+}
---
+++ server/pickaboo/src/test/java/com/project/pickaboo/PickabooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.pickaboo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PickabooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
