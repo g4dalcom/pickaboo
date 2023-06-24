04d0654f5ee1058a78e252b5db08a25c4c0d0e92
g4dalcom <g4dalcom@gmail.com>
2023-06-24 17:23:00 +0900

    chore: logging 작업

--- server/pickaboo/src/main/java/com/project/pickaboo/controller/MemberController.java
+++ server/pickaboo/src/main/java/com/project/pickaboo/controller/MemberController.java
@@ -1,15 +1,16 @@
 package com.project.pickaboo.controller;
 
 import com.project.pickaboo.dto.LoginDto;
-import com.project.pickaboo.dto.MemberDto;
 import com.project.pickaboo.dto.RegisterDto;
 import com.project.pickaboo.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 public class MemberController {
